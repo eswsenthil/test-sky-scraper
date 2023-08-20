d35c6e514ef080b2be7692bc65cf96d70ed3bdf2
eswsenthil <103065446+eswsenthil@users.noreply.github.com>
2023-08-20 22:38:52 +0300

    Improvements

--- test-sky-scrapper/PossibleValues.c
+++ test-sky-scrapper/PossibleValues.c
@@ -5,26 +5,28 @@
 //  Created by Tester on 20.8.2023.
 //
 
-#include "Position.h"
 #include "Constants.h"
 
 #include <errno.h>
 #include <stdio.h>
 
+const int gMinHeight = 1;
+const int gMaxHeight = 4;
+
 struct PossibleValues {
     int numValues;
-    int values[4];
+    int values[gMaxHeight];
 };
 
-struct PossibleValues gPossibleValues[4][4] = { 0 };
+struct PossibleValues gPossibleValues[gMaxHeight][gMaxHeight] = { 0 };
 
 extern int gRowLeft[gNumRows];
 extern int gRowRight[gNumRows];
 extern int gColUp[gNumCols];
 extern int gColDown[gNumCols];
 
-const int gMinHeight = 1;
-const int gMaxHeight = 4;
+int removeValueExcludingRowColumn(int rowIndex, int colIndex, int value);
+
 
 void initializePossibleValues(void)
 {
@@ -68,7 +70,7 @@ void printPossibleValues(void)
     }
 }
 
-int setValueForRowColumn(int rowIndex, int colIndex, int value)
+int findValueForRowColumn(int rowIndex, int colIndex, int value)
 {
     int index = 0;
     int found = 0;
@@ -88,8 +90,18 @@ int setValueForRowColumn(int rowIndex, int colIndex, int value)
         }
         printf("\n");
     }
-    gPossibleValues[rowIndex][colIndex].numValues = 1;
-    gPossibleValues[rowIndex][colIndex].values[0] = value;
+    return found;
+}
+
+int findExactInColumn(int colIndex, int value)
+{
+    int tmpRowIndex = 0;
+    while (tmpRowIndex < gNumRows) {
+        if ( (gPossibleValues[tmpRowIndex][colIndex].numValues == 1) &&
+            (gPossibleValues[tmpRowIndex][colIndex].values[0] == gMaxHeight)) {
+            return 1;
+        }
+    }
     return 0;
 }
 
@@ -132,13 +144,20 @@ int removeValueForRowColumn(int rowIndex, int colIndex, int value)
     }
     if (found == 1) {
         --gPossibleValues[rowIndex][colIndex].numValues;
+        
+        if (gPossibleValues[rowIndex][colIndex].numValues == 1) {
+            int valueToBeRemoved = gPossibleValues[rowIndex][colIndex].values[0];
+            removeValueExcludingRowColumn(rowIndex, colIndex, valueToBeRemoved);
+        }
     }
     
     return 0;
 }
 
-int removeValueForRowsAndColumns(int rowIndex, int colIndex, int value)
+int removeValueExcludingRowColumn(int rowIndex, int colIndex, int value)
 {
+    printf("removeValueExcludingRowColumn\n");
+    
     int tmpRowIndex = rowIndex + 1;
     while (tmpRowIndex < gNumRows) {
         removeValueForRowColumn(tmpRowIndex, colIndex, value);
@@ -161,8 +180,73 @@ int removeValueForRowsAndColumns(int rowIndex, int colIndex, int value)
         removeValueForRowColumn(rowIndex, tmpColIndex, value);
         --tmpColIndex;
     }
+    printPossibleValues();
+    
     return 0;
 }
+
+int setValueForRowColumn(int rowIndex, int colIndex, int value)
+{
+    printf("setValueForRowColumn\n");
+    int index = 0;
+    int found = 0;
+    while (index < gPossibleValues[rowIndex][colIndex].numValues) {
+        if (gPossibleValues[rowIndex][colIndex].values[index] == value) {
+            found = 1;
+            break;
+        }
+        ++index;
+    }
+    if (found == 0) {
+        printf("%d is not a possible value at (%d, %d). Available values are:", value, rowIndex, colIndex);
+        index = 0;
+        while (index < gPossibleValues[rowIndex][colIndex].numValues) {
+            printf("%d,", gPossibleValues[rowIndex][colIndex].values[index]);
+            ++index;
+        }
+        printf("\n");
+    }
+    gPossibleValues[rowIndex][colIndex].numValues = 1;
+    gPossibleValues[rowIndex][colIndex].values[0] = value;
+    printPossibleValues();
+    
+    removeValueExcludingRowColumn(rowIndex, colIndex, value);
+    
+    if (gRowLeft[rowIndex] == gMaxHeight && colIndex == (gMaxHeight-1)) {
+        int tmpColIndex = 0;
+        while (tmpColIndex < (gMaxHeight-1)) {
+            setValueForRowColumn(rowIndex, tmpColIndex, 1 + tmpColIndex);
+            ++tmpColIndex;
+        }
+    }
+    
+    if ((gRowRight[rowIndex] == gMaxHeight) && (rowIndex == 0)) {
+        int tmpColIndex = 1;
+        while (tmpColIndex < gMaxHeight) {
+            setValueForRowColumn(rowIndex, tmpColIndex, gMaxHeight - tmpColIndex);
+            ++tmpColIndex;
+        }
+    }
+    
+    if (gColUp[colIndex] == gMaxHeight && rowIndex == (gMaxHeight-1)) {
+        int tmpRowIndex = 0;
+        while (tmpRowIndex < (gMaxHeight-1)) {
+            setValueForRowColumn(tmpRowIndex, colIndex, 1 + tmpRowIndex);
+            ++tmpRowIndex;
+        }
+    }
+    
+    if ((gColDown[colIndex] == gMaxHeight) && (rowIndex == 0)) {
+        int tmpRowIndex = 1;
+        while (tmpRowIndex < (gMaxHeight-1)) {
+            setValueForRowColumn(tmpRowIndex, colIndex, gMaxHeight - tmpRowIndex);
+            ++tmpRowIndex;
+        }
+    }
+    
+    return 0;
+}
+
 
 int reduceMinAndMaxFromLeft(void)
 {
@@ -171,16 +255,16 @@ int reduceMinAndMaxFromLeft(void)
     
     while (rowIndex < gNumRows) {
         if (gRowLeft[rowIndex] == 1) {
-                setValueForRowColumn(rowIndex, colIndex, 4);
-                removeValueForRowsAndColumns(rowIndex, colIndex, 4);
-                printf("Handle gRowLeft[%d] == 1 for column 0\n", rowIndex);
-                printPossibleValues();
+            setValueForRowColumn(rowIndex, colIndex, 4);
+            removeValueExcludingRowColumn(rowIndex, colIndex, 4);
+            printf("Handle gRowLeft[%d] == 1 for column 0\n", rowIndex);
+            printPossibleValues();
         }
         else if (gRowLeft[rowIndex] == 4) {
             int tmpColIndex = colIndex;
             while (tmpColIndex < gNumCols) {
                 setValueForRowColumn(rowIndex, tmpColIndex, 1 + tmpColIndex);
-                removeValueForRowsAndColumns(rowIndex, tmpColIndex, 1 + tmpColIndex);
+                removeValueExcludingRowColumn(rowIndex, tmpColIndex, 1 + tmpColIndex);
                 ++tmpColIndex;
             }
             printf("Handle gRowLeft[%d] == 4 for column 0\n", rowIndex);
@@ -209,7 +293,7 @@ int reduceMinAndMaxFromRight(void)
     while (rowIndex < gNumRows) {
         if (gRowRight[rowIndex] == 1) {
             setValueForRowColumn(rowIndex, colIndex, 4);
-            removeValueForRowsAndColumns(rowIndex, colIndex, 4);
+            removeValueExcludingRowColumn(rowIndex, colIndex, 4);
             printf("Handle gRowRight[%d] == 1 for column 3\n", rowIndex);
             printPossibleValues();
         }
@@ -217,7 +301,7 @@ int reduceMinAndMaxFromRight(void)
             int tmpColIndex = 0;
             while (tmpColIndex < gNumCols) {
                 setValueForRowColumn(rowIndex, tmpColIndex, 1 + tmpColIndex);
-                removeValueForRowsAndColumns(rowIndex, tmpColIndex, 1 + tmpColIndex);
+                removeValueExcludingRowColumn(rowIndex, tmpColIndex, 1 + tmpColIndex);
                 ++tmpColIndex;
             }
             printf("Handle gRowRight[%d] == 4 for column 3\n", rowIndex);
@@ -246,7 +330,7 @@ int reduceMinAndMaxFromTop(void)
     while (colIndex < gNumCols) {
         if (gColUp[colIndex] == 1) {
                 setValueForRowColumn(rowIndex, colIndex, 4);
-                removeValueForRowsAndColumns(rowIndex, colIndex, 4);
+            removeValueExcludingRowColumn(rowIndex, colIndex, 4);
                 printf("Handle gColUp[%d] == 1\n", colIndex);
                 printPossibleValues();
         }
@@ -254,7 +338,7 @@ int reduceMinAndMaxFromTop(void)
             int tmpRowIndex = rowIndex;
             while (tmpRowIndex < gNumRows) {
                 setValueForRowColumn(tmpRowIndex, colIndex, 1 + tmpRowIndex);
-                removeValueForRowsAndColumns(tmpRowIndex, colIndex, 1 + tmpRowIndex);
+                removeValueExcludingRowColumn(tmpRowIndex, colIndex, 1 + tmpRowIndex);
                 ++tmpRowIndex;
             }
             printf("Handle gColUp[%d] == 4\n", colIndex);
@@ -283,16 +367,16 @@ int reduceMinAndMaxFromBottom(void)
     
     while (colIndex < gNumCols) {
         if (gColDown[colIndex] == 1) {
-                setValueForRowColumn(rowIndex, colIndex, 4);
-                removeValueForRowsAndColumns(rowIndex, colIndex, 4);
-                printf("Handle gColDown[%d] == 1\n", colIndex);
-                printPossibleValues();
+            setValueForRowColumn(rowIndex, colIndex, 4);
+            removeValueExcludingRowColumn(rowIndex, colIndex, 4);
+            printf("Handle gColDown[%d] == 1\n", colIndex);
+            printPossibleValues();
         }
         else if (gColDown[colIndex] == 4) {
             int tmpRowIndex = rowIndex;
             while (tmpRowIndex >= 0) {
                 setValueForRowColumn(tmpRowIndex, colIndex, 1 + tmpRowIndex);
-                removeValueForRowsAndColumns(tmpRowIndex, colIndex, 1 + tmpRowIndex);
+                removeValueExcludingRowColumn(tmpRowIndex, colIndex, 1 + tmpRowIndex);
                 --tmpRowIndex;
             }
             printf("Handle gColUp[%d] == 4\n", colIndex);
@@ -324,137 +408,149 @@ int reduceMinAndMax(void)
     return 0;
 }
 
-int guessMinIndexForTallestBuildingInForwardDirection(int numOfBuildingsVisible, int* minIndex, int *maxIndex)
+int guessIndicesForTallestBuilding(int numOfBuildingsVisible, int direction, int* minIndex, int *maxIndex)
 {
-    int minIndexForTallestBuilding = numOfBuildingsVisible - 1;
-    
+    *minIndex = numOfBuildingsVisible - 1;
+        
     if ((numOfBuildingsVisible == 1) ||
         (numOfBuildingsVisible == gMaxHeight)) {
-        *minIndex = minIndexForTallestBuilding;
         *maxIndex = *minIndex;
     }
-    
-    
-    return 0;
-}
-
-int reduceOtherFromLeft(void)
-{
-    int rowIndex = 0;
-    while (rowIndex < gNumRows) {
-        
+    else {
+        *maxIndex = gMaxHeight - 1;
     }
+    
+    if (direction == -1) {
+        *minIndex = gMaxHeight - *minIndex - 1;
+        *maxIndex = gMaxHeight - *maxIndex - 1;
+     }
     return 0;
 }
 
-int iteration1(void)
+int guessLeastColumnIndex(int rowIndex, int minColIndex, int maxColIndex, int *bestIndex)
 {
-    printf("Iteration 1:\n");
+    int foundColIndex = -1;
+    int foundCount = 0;
+    int colIndex = maxColIndex;
+    while (colIndex >= minColIndex) {
+        int found = findValueForRowColumn(rowIndex, colIndex, gMaxHeight);
+        if (found) {
+            ++foundCount;
+            foundColIndex = colIndex;
+//            TODO - check if any other entries in the same column
+//            has the possible value set with gMaxHeight
+        }
+        --colIndex;
+    }
+    if (foundCount == 1) {
+        *bestIndex = foundColIndex;
+        return 1;
+    }
+    return -1;
+}
+
+int guessLeastRowIndex(int colIndex, int minRowIndex, int maxRowIndex, int *bestIndex)
+{
+    int foundRowIndex = -1;
+    int foundCount = 0;
+    int rowIndex = maxRowIndex;
+    while (rowIndex >= minRowIndex) {
+        int found = findValueForRowColumn(rowIndex, colIndex, gMaxHeight);
+        if (found) {
+            ++foundCount;
+            foundRowIndex = rowIndex;
+//            TODO - check if any other entries in the same row
+//            has the possible value set with gMaxHeight
+        }
+        --rowIndex;
+    }
+    if (foundCount == 1) {
+        *bestIndex = foundRowIndex;
+        return 1;
+    }
+    return -1;
+}
+
+int reduceFromRowsClue(int *visibleBuildings, int direction, int allowedValue)
+{
+    printf("reduceOthersFromLeft\n");
+    
+    int minIndex;
+    int maxIndex;
     
     int rowIndex = 0;
     while (rowIndex < gNumRows) {
-        int colIndex = 0;
-        while (colIndex < gNumCols) {
-            if (gPossibleValues[rowIndex][colIndex].numValues != 1) {
-                
-            }
-            ++colIndex;
+        if (visibleBuildings[rowIndex] != allowedValue) {
+            ++rowIndex;
+            continue;
         }
-        printf("\n");
+        minIndex = -1;
+        maxIndex = -1;
+        guessIndicesForTallestBuilding(visibleBuildings[rowIndex], direction, &minIndex, &maxIndex);
+        if (minIndex == maxIndex) {
+            setValueForRowColumn(rowIndex, minIndex, gMaxHeight);
+        }
+        else {
+            int bestIndex = -1;
+            int found = guessLeastColumnIndex(rowIndex, minIndex, maxIndex, &bestIndex);
+            if (found == 1) {
+                setValueForRowColumn(rowIndex, bestIndex, gMaxHeight);
+            }
+        }
         ++rowIndex;
     }
+    printPossibleValues();
+    return 0;
+}
+
+int reduceFromColumnsClue(int *visibleBuildings, int direction, int allowedValue)
+{
+    printf("reduceFromColumnsClue\n");
+    
+    int minIndex;
+    int maxIndex;
+    
+    int colIndex = 0;
+    while (colIndex < gNumCols) {
+        if (visibleBuildings[colIndex] != allowedValue) {
+            ++colIndex;
+            continue;
+        }
+        minIndex = -1;
+        maxIndex = -1;
+        guessIndicesForTallestBuilding(visibleBuildings[colIndex], direction, &minIndex, &maxIndex);
+        if (minIndex == maxIndex) {
+            setValueForRowColumn(minIndex, colIndex, gMaxHeight);
+        }
+        else {
+            int bestIndex = -1;
+            int found = guessLeastRowIndex(colIndex, minIndex, maxIndex, &bestIndex);
+            if (found == 1) {
+                setValueForRowColumn(bestIndex, colIndex, gMaxHeight);
+            }
+        }
+        ++colIndex;
+    }
+    printPossibleValues();
     return 0;
 }
 
 int solve(void)
 {
-    reduceMinAndMax();
+//    reduceMinAndMax();
     
-    iteration1();
+    int clueOrder[4] = { 4, 1, 3, 2 };
+    int clueIndex = 0;
+    while (clueIndex < 4) {
+        reduceFromRowsClue(gRowLeft, 1, clueOrder[clueIndex]);
+        reduceFromRowsClue(gRowRight, -1, clueOrder[clueIndex]);
+        reduceFromColumnsClue(gColUp, 1, clueOrder[clueIndex]);
+        reduceFromColumnsClue(gColDown, -1, clueOrder[clueIndex]);
+        ++clueIndex;
+    }
+    
+//    iteration1();
     return 0;
 }
 
-int setExpectedValuesForRow(int rowIndex)
-{
-    switch (gRowLeft[rowIndex]) {
-        case 1:
-        {
-            if (rowIndex == 0 && gColUp[0] != 1) {
-                printf("gRowLeft[0] is 1 but gColUp[0] is not 1\n");
-                return EINVAL;
-            }
-            
-            if (rowIndex == 3 && gColDown[0] != 1) {
-                printf("gRowLeft[3] is 1 but gColDown[0] is not 1\n");
-                return EINVAL;
-            }
-            gPossibleValues[rowIndex][0].numValues = 1;
-            gPossibleValues[rowIndex][0].values[0] = 4;
-            int colIndex = 1;
-            
-            break;
-        }
-            
-        case 2:
-        {
-            break;
-        }
-        
-        case 3:
-        {
-            break;
-        }
-        
-        case 4:
-        {
-            if (gRowRight[rowIndex] != 1) {
-                printf("gRowLeft[rowIndex] is 4 but gRowRight[rowIndex] is not 1 for rowIndex:%d\n", rowIndex);
-                return EINVAL;
-            }
-            
-            int colIndex = 0;
-            while (colIndex < 4) {
-                gPossibleValues[rowIndex][colIndex].numValues = 1;
-                gPossibleValues[rowIndex][colIndex].values[0] = 1 + colIndex;
-                ++colIndex;
-            }
-            
-            break;
-        }
-    }
-    return 0;
-}
-
-int initializeMatrix(void)
-{
-    struct Position *position = gHeadPosition;
-    while (position) {
-        int rowIndex = position->row;
-        int colIndex = position->column;
-        
-    }
-    
-    int rowIndex = 0;
-    while (rowIndex < gNumRows) {
-        switch (gRowLeft[rowIndex]) {
-            case 1:
-                {
-                    int colIndex = 0;
-                    while (colIndex < 4) {
-                        gPossibleValues[rowIndex][colIndex].numValues = 1;
-                        gPossibleValues[rowIndex][colIndex].values[0] = 4 - colIndex;
-                        ++colIndex;
-                    }
-                    if (rowIndex == 0) {
-                        
-                    }
-                }
-                break;
-                
-            default:
-                break;
-        }
-    }
-    return 0;
-}
 
